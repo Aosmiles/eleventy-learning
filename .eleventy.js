45ee6220aaeb4d3c2120dbce3e3bc5c2d5bf055f
Aosmiles <arlin.smiles@gmail.com>
2022-08-14 18:27:58 -0700

    update config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 ﻿module.exports = function (eleventyConfig) {
+  
+  eleventyConfig.addPassthroughCopy("./src/css/style.css")
+  eleventyConfig.addPassthroughCopy("./src/assets/")
+  
   return {
     dir: {
       input: "src",
