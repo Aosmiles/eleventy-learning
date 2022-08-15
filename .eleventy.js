320f24e1740bf838f3052ef2cdc91cf29236bcb5
Aosmiles <arlin.smiles@gmail.com>
2022-08-14 19:24:22 -0700

    rename files to njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,17 @@
 ﻿module.exports = function (eleventyConfig) {
   
-  eleventyConfig.addPassthroughCopy("./src/css/style.css")
-  eleventyConfig.addPassthroughCopy("./src/assets/")
+  eleventyConfig.addPassthroughCopy('./src/css/style.css')
+  eleventyConfig.addPassthroughCopy('./src/assets/')
   
   return {
     dir: {
-      input: "src",
-      output: "dist"
-    }
-  }
+      input: 'src',
+      includes: '_includes',
+      output: 'dist',
+    },
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dateTemplateEngine: 'njk',
+  };
 }
